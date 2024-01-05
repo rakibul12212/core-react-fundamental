9bba1b89adc25a334dc30db10444faa0b1723cac
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-05 14:52:23 -0800

    first commit

---
+++ src/another.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const HalloWorld = () => {
+  return React.createElement("p", {}, "Hello World");
+};
+export const AnotherHello = () => {
+  return React.createElement("p", {}, "Another Hello");
+};
